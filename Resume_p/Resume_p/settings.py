bec81ec9521df66573b37e6fe5d6893dd1e6b2f2
jagadeeswarudu <96833531+jagadeeswarudu@users.noreply.github.com>
2024-02-05 15:08:38 +0530

    Update settings.py

--- Resume_p/Resume_p/settings.py
+++ Resume_p/Resume_p/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path,os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,6 +126,8 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+UPLOADS_PATH = os.path.join(MEDIA_ROOT, 'uploads')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
