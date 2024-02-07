ca9f91052243c150844dc5d79cb98de9b3147af8
jagadeeswarudu <96833531+jagadeeswarudu@users.noreply.github.com>
2024-02-07 15:56:19 +0530

    Update views.py

--- Resume_p/calc/views.py
+++ Resume_p/calc/views.py
@@ -14,6 +14,7 @@ import logging
 
 from .models import Contact
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 UPLOADS_PATH = os.path.join(MEDIA_ROOT, 'uploads')
 
